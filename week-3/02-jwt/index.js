c8822e6b9f66a30b6cad760b2b743aae687f81a0
Pruthvi Purohit <pruthvipurohit@gmail.com>
2024-03-05 05:29:20 +0530

    Solution for week 3 JWT Assignment

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken');
+const zod = require('zod');
 const jwtPassword = 'secret';
 
-
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 /**
  * Generates a JWT for a given username and password.
  *
@@ -15,6 +17,15 @@ const jwtPassword = 'secret';
  */
 function signJwt(username, password) {
     // Your code here
+    const usernameResponse = emailSchema.safeParse(username);
+    const passwordResponse = passwordSchema.safeParse(password);
+
+    if(!usernameResponse.success || !passwordResponse.success){
+        return null;
+    }
+
+    const signature = jwt.sign({username}, jwtPassword);
+    return signature;
 }
 
 /**
@@ -27,6 +38,12 @@ function signJwt(username, password) {
  */
 function verifyJwt(token) {
     // Your code here
+    try{
+        jwt.verify(token, jwtPassword);
+        return true;
+    } catch(e){
+        return false;
+    }
 }
 
 /**
@@ -38,6 +55,12 @@ function verifyJwt(token) {
  */
 function decodeJwt(token) {
     // Your code here
+    const jwtResponse = jwt.decode(token);
+    if(jwtResponse){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 
