06851607eba32bd9c426f26c6a2c699d06f9db77
Pruthvi Purohit <pruthvipurohit@gmail.com>
2024-03-06 06:27:22 +0530

    MongoDB assignments Course Selling Application without JWT Authentication

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,23 @@
 // Middleware for handling auth
+const { Admin } = require("../db");
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    username = req.headers.username;
+    password = req.headers.password;
+
+    Admin.findOne({
+        username: username,
+        password: password
+    }).then(function(value) {
+        if(value){
+            next();
+        }else{
+            res.status(500).json({
+                msg: "Admin not found"
+            });
+        }
+    });
 }
 
 module.exports = adminMiddleware;
--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,23 @@
+const { User } = require("../db");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    username = req.headers.username;
+    password = req.headers.password;
+
+    User.findOne( {
+        username: username,
+        password: password
+    }).then(function (user) {
+        if(user){
+            next();
+        }else{
+            res.status(500).json({
+                msg: "User not found"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
